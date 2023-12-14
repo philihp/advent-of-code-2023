3ca1672c30f8d8f9f0b9122bc7ac22c85cd9a0ef
Philihp Busby <philihp@gmail.com>
2023-12-14 22:43:26 +0000

    pipe console.log

--- src/day14/part1.js
+++ src/day14/part1.js
@@ -3,21 +3,20 @@ import { reverse, sortBy, identity, join, split, transpose, sum, pipe, map, addI
 // import input from './test.js'
 import input from './real.js'
 
-console.log(
-  pipe(
-    transpose,
-    map(
-      pipe(
-        join(''),
-        split('#'),
-        map((s) => join('', reverse(sortBy(identity, s)))),
-        join('#')
-      )
-    ),
-    transpose,
-    map(join('')),
-    addIndex(map)((s, i) => [s.split('O').length - 1, input.length - i]),
-    map(([a, b]) => a * b),
-    sum
-  )(input)
-)
+pipe(
+  transpose,
+  map(
+    pipe(
+      join(''),
+      split('#'),
+      map((s) => join('')(reverse(sortBy(identity, s)))),
+      join('#')
+    )
+  ),
+  transpose,
+  map(join('')),
+  addIndex(map)((s, i) => [s.split('O').length - 1, input.length - i]),
+  map(([a, b]) => a * b),
+  sum,
+  console.log
+)(input)
